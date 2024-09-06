8a38538f4cdcec1e7b2edd1931994456629d31a9
lixiangcloud <97663175+lixiangcloud@users.noreply.github.com>
2024-09-06 17:43:35 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["36.153.42.194:21116"];
+pub const PUBLIC_RS_PUB_KEY: &str = "TWH4bR2VtY43nCm4N1liXRPfDH0F55+KeWbCW8eQLTo=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
